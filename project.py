aaec2e7bbc6532045cfc6b26e5f5e7243a9a0c33
Rafal Wyszynski <rafalwys@onet.pl>
2023-01-16 00:40:33 +0100

    Define input

---
+++ project.py
@@ -0,0 +1,15 @@
+def QuickSort(arr):
+    arr2 = arr
+    n = len(arr)
+    
+
+
+
+array = []
+print("Ile danych chcesz wprowadzić?")
+x = int(input())
+print("Wprowadź " +str(x) +" wartości. Każdą potwierdź spacją:")
+for i in range (0,x):
+    c = int(input())
+    array.append(c)    
+print(array)
