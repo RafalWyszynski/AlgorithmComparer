29d42087d257a5095809a11114150e7b16475fb9
Rafal Wyszynski <rafalwys@onet.pl>
2023-01-16 22:35:14 +0100

    Add quickSort and randomQuickSort

--- project.py
+++ project.py
@@ -21,6 +21,26 @@ def quickSort(arr, left, right):
     quickSort(arr, left, j)
     quickSort(arr, j+1, right)
 
+def randomQuickSort(arr, left, right):
+    if(left >= right):
+        return
+    pivot = arr[random.randint(left,right)]  
+    i = left - 1
+    j = right + 1
+    while(1):
+        while(1):             
+            i += 1
+            if(arr[i] >= pivot):
+                break
+        while(1):           
+            j -= 1
+            if(arr[j] <= pivot):
+                break
+        if(i >= j):
+            break
+        arr[i],arr[j] = arr[j],arr[i]
+    quickSort(arr, left, j)
+    quickSort(arr, j+1, right)
 
 array = []
 print("Ile danych chcesz wprowadzić?")
@@ -28,7 +48,11 @@ x = int(input())
 print("Wprowadź " +str(x) +" wartości. Każdą potwierdź spacją:")
 for i in range (0,x):
     c = int(input())
-    array.append(c)    
+    array.append(c)  
+array2 = array.copy()
 print(array)
+
 quickSort(array, 0, len(array)-1)
+quickSort(array2, 0, len(array2)-1)
 print(array)
+print(array2)
