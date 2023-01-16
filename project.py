50fb8c7c6f9ba2f7f3d6773404e1d3778e74ed77
Rafal Wyszynski <rafalwys@onet.pl>
2023-01-16 03:40:27 +0100

    QuickSort Almost Finished

--- project.py
+++ project.py
@@ -1,10 +1,31 @@
-def QuickSort(arr):
-    arr2 = arr
-    n = len(arr)
+import random
+
+def QuickSort(arr, left, right):
+    if left < right:
+        x = random.randint(left,right)
+        print(x)
+        i = left
+        j = right
+        while 1>0:
+            while arr[i] < arr[x] and i<len(arr):
+                i = i+1
+            while arr[j] >= arr[x] and j>=0:
+                j = j-1
+            if i <= j:
+                arr[i],arr[j] = arr[j],arr[i]
+            else:
+                break
+        print(arr)              
+        if i < right:
+            QuickSort(arr, i, right)
+        if j > left:
+            QuickSort(arr, left, j)
+
+
     
 
 
-
+    
 array = []
 print("Ile danych chcesz wprowadzić?")
 x = int(input())
@@ -13,3 +34,4 @@ for i in range (0,x):
     c = int(input())
     array.append(c)    
 print(array)
+print(QuickSort(array, 0, len(array)-1))
